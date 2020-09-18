63d67b2ea9a991684e8ebf30462cb92222dfdca6
petrero <petrero@gmail.com>
2020-09-18 11:38:29 +0300

    The second way to create a dynamic route without limiting the flexibility

--- src/views/User.vue
+++ src/views/User.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="user">
-    <h1>This is {{ $route.params.username }}'s page</h1>
+    <h1>This is {{ username }}'s page</h1>
   </div>
 </template>
 
 <script>
 export default {
-name: "User"
+  name: "User",
+  props: ["username"],
 }
 </script>
 
