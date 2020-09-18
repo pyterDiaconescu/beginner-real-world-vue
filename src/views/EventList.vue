fc925bc9ade251e4d1c8f7741057773b37e99d3c
petrero <petrero@gmail.com>
2020-09-18 11:14:18 +0300

    4.4. Vue Router Basics ---Create the skeleton of our real application

---
+++ src/views/EventList.vue
@@ -0,0 +1,13 @@
+<template>
+ <h1>Events List</h1>
+</template>
+
+<script>
+export default {
+name: "EventList"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/EventShow.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Events Show</h1>
+</template>
+
+<script>
+export default {
+name: "EventShow"
+}
+</script>
+
+<style scoped>
+
+</style>
